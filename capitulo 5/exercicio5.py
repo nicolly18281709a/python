a32a3a9f9dda5258bd7318c20485b8ae6a44bfec
gustavo <gustavo.lima04@escola.pr.gov.br>
2025-09-19 12:14:29 -0300

    capitulo 5

---
+++ capitulo 5/exercicio5.py
@@ -0,0 +1,15 @@
+def calculaQuantidadePares(numero1, numero2, numero3, numero4):
+    quantidadePar = 0
+
+    if(numero1 % 2 == 0):
+        quantidadePar += 1
+    if(numero2 % 2 == 0):
+        quantidadePar += 1
+    if(numero3)
+
+numero1 = int(input("digite o primeiro numero:"))
+numero2 = int(input("digite o segundo numero:"))
+numero3 = int(input("digite o terceito numero:"))
+numero4 = int(input("digite o quarto numero:"))
+
+quan
